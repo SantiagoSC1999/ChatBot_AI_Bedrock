b1e345e6d3cddb3311fd894be13ddf370f8f1926
SantiagoSC1999 <sasa.sanchezcorre-7@hotmail.com>
2025-08-25 16:27:25 -0500

    Split Saving and supabase create client

--- ai_chatbot/app/llm/store_vectors.py
+++ ai_chatbot/app/llm/store_vectors.py
@@ -8,20 +8,6 @@ logger = get_logger()
 
 load_dotenv()
 
-def setup_supabase():
-    print('entrando a configuración de supabase')
-    DB_CONNECTION = os.getenv('DB_SUPABASE_CONNECTION')
-    COLLECTION_NAME = os.getenv('SUPABASE_COLLECTION')
-    
-        # Crear cliente
-    try:
-        vx = vecs.create_client(DB_CONNECTION)
-        collection = vx.get_or_create_collection(name=COLLECTION_NAME, dimension=1024)
-        logger.info(f" Colección '{COLLECTION_NAME}' obtenida")
-        return collection
-    except Exception as e:
-        logger.error(f" Error con Supabase: {str(e)}")
-           # return None
 
 def store_embedding(collection, text: str, embedding: list):
     try:   
